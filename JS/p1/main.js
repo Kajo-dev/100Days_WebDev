1e7144e4b3b96f9e17a687264eaaf7fa0854a9c3
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-10-16 21:55:45 +0200

    zmiana kolorow

--- JS/p1/main.js
+++ JS/p1/main.js
@@ -6,8 +6,16 @@ let cur_char = 0
 
 function returnUser(event){
     let value_event = event.target.value
+    
     value_event.toString
     paragraf.innerHTML = value_event.length + '/' + maxchar
+
+    if (value_event.length >60 ){
+        paragraf.style.color = "red"
+    }
+    else{
+        paragraf.style.color = "black"
+    }
 }
 
 inp.addEventListener('input',returnUser)
