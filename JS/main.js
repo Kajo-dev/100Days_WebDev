62d3f6807ec3ed70520977dda5c1d1ea74103af0
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-10-16 19:00:13 +0200

    js innerHtml

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,26 @@
-console.dir(window.document);
+//create editing
+
+let nowy = document.createElement('a')
+
+nowy.href = 'kajo'
+nowy.textContent = 'super'
+
+let link = document.getElementById('link')
+
+link.append(nowy)
+
+//remove
+
+let h1 = document.querySelector('h1')
+
+h1.remove()
+
+//move 
+
+let paragraf = document.querySelector('p')
+
+paragraf.parentElement.append(paragraf)
+
+// innerHTML
+
+paragraf.innerHTML
